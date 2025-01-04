f41c28d81e8ab7bc0c7e175bde2bff40da172460
Gerard TE <supa.gei.gei@outlook.com>
2025-01-04 14:39:09 +0100

    iterators/iterator2

---
+++ exercises/10_modules/modules2.rs
@@ -0,0 +1,29 @@
+// You can bring module paths into scopes and provide new names for them with
+// the `use` and `as` keywords.
+
+mod delicious_snacks {
+    // TODO: Add the following two `use` statements after fixing them.
+    // use self::fruits::PEAR as ???;
+    // use self::veggies::CUCUMBER as ???;
+
+    pub mod fruits {
+        pub const PEAR: &str = "Pear";
+        pub const APPLE: &str = "Apple";
+    }
+
+    pub mod veggies {
+        pub const CUCUMBER: &str = "Cucumber";
+        pub const CARROT: &str = "Carrot";
+    }
+}
+
+fn main() {
+    use delicious_snacks :: fruits:: PEAR as pear;
+    use delicious_snacks :: veggies:: CARROT as carrot;
+
+    println!(
+        "favorite snacks: {} and {}",
+        pear,
+        carrot,
+    );
+}
---
+++ exercises/17_tests/tests3.rs
@@ -0,0 +1,58 @@
+struct Rectangle {
+    width: i32,
+    height: i32,
+}
+
+impl Rectangle {
+    // Don't change this function.
+    fn new(width: i32, height: i32) -> Self {
+        if width <= 0 || height <= 0 {
+            // Returning a `Result` would be better here. But we want to learn
+            // how to test functions that can panic.
+            panic!("Rectangle width and height must be positive");
+        }
+
+        Rectangle { width, height }
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::panic;
+
+    #[test]
+    fn correct_width_and_height() {
+        // TODO: This test should check if the rectangle has the size that we
+        // pass to its constructor.
+        let rect = Rectangle::new(10, 20);
+        assert_eq!(rect.width, 10); // Check width
+        assert_eq!(rect.height, 20); // Check height
+    }
+
+    // TODO: This test should check if the program panics when we try to create
+    // a rectangle with negative width.
+    #[test]
+    fn negative_width() {
+        let result = panic::catch_unwind(|| {
+            Rectangle::new(-10, 10)// This panics
+        });
+
+        assert!(result.is_err());
+    }
+
+    // TODO: This test should check if the program panics when we try to create
+    // a rectangle with negative height.
+    #[test]
+    fn negative_height() {
+        let result = panic::catch_unwind(|| {
+            Rectangle::new(-10, 10)// This panics
+        });
+
+        assert!(result.is_err());
+    }
+}
